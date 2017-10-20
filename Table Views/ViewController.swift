3084a06570484c792e14441add9d47ee925e2a6f
jackson meyer <jackson.meyer7@gmail.com>
2017-10-20 13:00:33 -0700

    used table view to display integers 1-50 in rows

--- Table Views/ViewController.swift
+++ Table Views/ViewController.swift
@@ -10,22 +10,25 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    let cellContent = ["bren", "Megan", "Max", "Henry"]
+    var number = 0
     
-    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return cellContent.count
+        return 50
+        
     }
     
-    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
         
-        cell.textLabel?.text = cellContent[indexPath.row]
+        cell.textLabel?.text = String(indexPath.row + 1)
         
         return cell
+        
     }
     
+
 
 
     override func viewDidLoad() {
