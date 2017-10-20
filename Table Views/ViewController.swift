967a8b8496d846a84882d76a734d3fce5a32c6b3
jackson meyer <jackson.meyer7@gmail.com>
2017-10-20 12:49:15 -0700

    TableView 2 methods, and added some code for Index Paths

--- Table Views/ViewController.swift
+++ Table Views/ViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let cellContent = ["bren", "Megan", "Max", "Henry"]
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return cellContent.count
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = cellContent[indexPath.row]
+        
+        return cell
+    }
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
